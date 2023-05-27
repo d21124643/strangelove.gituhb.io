cff8c3741c2395973eb5c5523f280bf5d9aac723
d21124643 <102530923+d21124643@users.noreply.github.com>
2023-05-27 21:31:37 +0100

    Add files via upload

--- api_script.js
+++ api_script.js
@@ -1,23 +1,10 @@
-// Fetch weather data
-fetch('https://api.open-meteo.com/v1/latitude=51.90&longitude=-8.47&hourly=temperature_2m,precipitation,weathercode,pressure_msl&current_weather=true')
-  .then(response => response.json())
-  .then(data => {
-    // Process the weather data
-    const currentWeather = data.current_weather;
-    const temperature = currentWeather.temperature_2m.value;
-    const precipitation = currentWeather.precipitation.value;
-    const weatherCode = currentWeather.weathercode.value;
-    const pressure = currentWeather.pressure_msl.value;
-
-    // Display the weather data
-    const weatherContainer = document.getElementById('weatherContainer');
+fetch('https://api.open-meteo.com/v1/forecast?latitude=51.90&longitude=8.47&current_weather=true')
+.then(response => response.json())
+.then(data => {
+    const weatherContainer = document.getElementById("weather-data");
     weatherContainer.innerHTML = `
-      <p>Temperature: ${temperature}°C</p>
-      <p>Precipitation: ${precipitation} mm</p>
-      <p>Weather Code: ${weatherCode}</p>
-      <p>Pressure: ${pressure} hPa</p>
+    <p>Temperature: ${data.current_weather.temperature}°C</p>
+    <p>Precipitation probability: ${data.current_weather.precipitation_probability}</p>
+    <p>Cloud cover: ${data.current_weather.cloudcover}
     `;
-  })
-  .catch(error => {
-    console.error('Error fetching weather data:', error);
-  });
+});
